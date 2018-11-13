9ef0469817d58ccc7625a78a178534b317b1ff50
yuanxuechunshr <805169158@qq.com>
2018-11-13 13:37:28 +0800

    第一次发送

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -19,6 +19,7 @@ public class App {
 
         @Bean
         public HttpMessageConverters fastjsonHttpMessageConverter(){
+            System.out.println("aaaaaaaa");
         //定义一个转换消息的对象
             FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
      //添加fastjson的配置信息 比如 ：是否要格式化返回的json数据
